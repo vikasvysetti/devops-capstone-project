a6570b6548a994c19e74fe980a21b44d2575ea5b
Vikas Vysetti <vysetti1987@gmail.com>
2024-02-09 14:38:33 -0500

    test case to list all accounts

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -141,3 +141,12 @@ class TestAccountService(TestCase):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_get_account_list(self):
+        """It should list all accounts"""
+        self._create_accounts(6)
+        response = self.client.get(BASE_URL)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(len(data), 6)
+
