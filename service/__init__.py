76b20948143301532c3487d79da3c0e9321f0048
vikasvysetti <vysetti1987@gmail.com>
2024-02-09 16:45:56 -0500

    Added security headers

--- service/__init__.py
+++ service/__init__.py
@@ -5,6 +5,7 @@ This module creates and configures the Flask app and sets up the logging
 and SQL database
 """
 import sys
+from flask_talisman import Talisman
 from flask import Flask
 from service import config
 from service.common import log_handlers
@@ -13,6 +14,7 @@ from service.common import log_handlers
 app = Flask(__name__)
 app.config.from_object(config)
 
+talisman = Talisman(app)
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
 from service import routes, models  # noqa: F401 E402
